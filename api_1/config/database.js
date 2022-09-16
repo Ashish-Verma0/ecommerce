05bb648a65cc3a069e7b5bea44d8742935c20bf8
Ashish Verma <av84770@gmail.com>
2022-09-16 14:15:24 +0530

    sdbusd

---
+++ api_1/config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+mongoose.connection.once('open', () => {
+    console.log('database connected')
+})
+
+mongoose.connection.on('err', () => {
+    console.log('err')
+})
+
+// databaseUrl = "mongodb+srv://Ecommerce:wdc07HIzMoOUQtcn@cluster0.3hdmt.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+console.log(process.env.DB_URI)
+const connectDatabase = () => {
+
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = connectDatabase
